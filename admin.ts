6491f0aa0e9787f2fe9eb903f6ee84a1cb00d19c
munibaweb123 <159945886+munibaweb123@users.noreply.github.com>
2024-02-27 22:48:05 +0500

    Add files via upload

--- admin.ts
+++ admin.ts
@@ -1,4 +1,5 @@
 let usernames=["Admin","Sir Danyal Nigori","Sir Kamran Khan Tessori","Sir Hamza","Sir Zia Khan"];
+
 for(let i=0;i<usernames.length;i++){
     if(usernames[i]=="Admin"){
         console.log("Hello admin, would you like to see a status report?")
@@ -6,4 +7,14 @@ for(let i=0;i<usernames.length;i++){
     else{
         console.log(" Hello "+usernames[i]+", thank you for logging in again.")
     }
+}
+
+for(let i=0;i<usernames.length;i++){
+    if(usernames[i]==""){
+        console.log("We need to find some users!?")
+    }
+    else{
+        usernames=[]
+    }
+    console.log("We need to find some users!?")
 }
