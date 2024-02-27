90ee26dd3c2026d521eb03919068c4b81722958b
munibaweb123 <159945886+munibaweb123@users.noreply.github.com>
2024-02-27 23:09:20 +0500

    Add files via upload

--- storeinarr.ts
+++ storeinarr.ts
@@ -1,18 +1,29 @@
-//21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+// else you’d like. Write a program that creates a list containing these items.
 
-var obj={Mountain1:"Mount Averest",mountain2:"K-2",mountain3:"Nanga Parbat",mountain4:"Karakoram"}
-var obj2={River1:"Nile",River2:"Amazon",River3:"Yangtze",River4:"Yenisei"}
-var obj3={City1:"Karachi",City2:"Hyderabad",City3:"Islamabad",City4:"Lahore",City5:"Nawabshah"}
-var obj4={Country1:"Pakistan",Country2:"Bangladesh",Country3:"China",Country4:"Saudia Arabia",Country5:"India",Country6:"Afghanistan"}
+
+var mountains=["Mount Averest","K-2","Nanga Parbat","Karakoram"];
+var rivers=["Nile","Amazon","Yangtze","Yenisei"];
+var cities=["Karachi","Hyderabad","Islamabad","Lahore","Nawabshah"];
+var countries=["Pakistan","Bangladesh","Saudia Arabia","India","Afghanistan","China"];
+
 //List of mountains!
 console.log("\n List of Mountains!\n");
-console.log("\n1",obj.Mountain1,"\n2",obj.mountain2,"\n3",obj.mountain3,"\n4",obj.mountain4);
+for(var i=0;i<mountains.length;i++){
+    console.log(i+1+"\t"+mountains[i]);
+}
 //List of Rivers!
 console.log("\n List of Rivers!\n");
-console.log("\n1",obj2.River1,"\n2",obj2.River2,"\n3",obj2.River3,"\n4",obj2.River4);
+for( i=0;i<rivers.length;i++){
+    console.log(i+1+"\t"+rivers[i]);
+}
 //List of Cities!
 console.log("\n List of Cities!\n");
-console.log("\n1",obj3.City1,"\n2",obj3.City2,"\n3",obj3.City3,"\n4",obj3.City4,"\n5",obj3.City4,"\n5",obj3.City5);
+for( i=0;i<cities.length;i++){
+    console.log(i+1+"\t"+cities[i]);
+}
 //List of Countries!
 console.log("\n List of Countries!\n");
-console.log("\n1",obj4.Country1,"\n2",obj4.Country2,"\n3",obj4.Country3,"\n4",obj4.Country4,"\n5",obj4.Country5,"\n5",obj4.Country6);
+for( i=0;i<countries.length;i++){
+    console.log(i+1+"\t"+countries[i]);
+}
